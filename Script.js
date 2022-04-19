78b4585df6f5895e4a725ac8162335c9041a47b1
AlexxyQQ <aayushpandey616@gmail.com>
2022-04-19 19:01:08 +0545

    phone recording animation polished somewhat

--- Script.js
+++ Script.js
@@ -19,21 +19,21 @@ function Listening() {
     setTimeout(function () {
       document.getElementById("listening-btn").disabled = false;
       x.className = "listening-btn";
-    }, 5000);
+    }, 10000);
   }
   if (y.className === "listening-btn-layer1") {
     y.className += " listening";
     setTimeout(function () {
       document.getElementById("listening-btn-layer1").disabled = false;
       y.className = "listening-btn-layer1";
-    }, 5000);
+    }, 10000);
   }
   if (z.className === "listening-btn-layer2") {
     z.className += " listening";
     setTimeout(function () {
       document.getElementById("listening-btn-layer2").disabled = false;
       z.className = "listening-btn-layer2";
-    }, 5000);
+    }, 10000);
   }
   if (a.className === "listening-btn-layer3") {
     document.getElementById("listening-btn-layer3").disabled = true;
@@ -41,7 +41,7 @@ function Listening() {
       document.getElementById("listening-btn-layer3").disabled = false;
       a.className += " listening";
       a.className = "listening-btn-layer3";
-    }, 5000);
+    }, 10000);
   } else {
     a.className = "listening-btn-layer3";
   }
@@ -51,12 +51,31 @@ function smalllsn() {
   var b = document.getElementById("lb");
 
   if (b.className === "listening-bars") {
-    b.className += " ras";
+    b.className += " listening";
+    document.getElementById("one").className += " listening";
+    document.getElementById("two").className += " listening";
+    document.getElementById("three").className += " listening";
+    document.getElementById("four").className += " listening";
+    document.getElementById("five").className += " listening";
+    document.getElementById("six").className += " listening";
+    document.getElementById("seven").className += " listening";
+    document.getElementById("eight").className += " listening";
+    document.getElementById("gooey").className += " listening";
+
     document.getElementById("rs").disabled = true;
     setTimeout(function () {
       document.getElementById("rs").disabled = false;
       b.className = "listening-bars";
-    }, 5000);
+      document.getElementById("one").className += "one";
+      document.getElementById("two").className += "two";
+      document.getElementById("three").className += "three";
+      document.getElementById("four").className += "four";
+      document.getElementById("five").className += "five";
+      document.getElementById("six").className += "six";
+      document.getElementById("seven").className += "seven";
+      document.getElementById("eight").className += "eight";
+      document.getElementById("gooey").className += "gooey";
+    }, 10000);
   }
 }
 
