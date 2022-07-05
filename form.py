4bd7345ad76a11a64a428423b7811b1ebe34fc2a
AlexxyQQ <aayushpandey616@gmail.com>
2022-07-05 21:06:52 +0545

    user account page started

--- form.py
+++ form.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from models import user as User
 
@@ -35,3 +37,24 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    # email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField("", validators=[FileAllowed(["png"])])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("That username is taken.")
+
+    # def validate_email(self, email):
+    #     if email.data != current_user.email:
+    #         user = User.query.filter_by(email=email.data).first()
+    #         if user:
+    #             raise ValidationError("That email is taken.")
--- view.py
+++ view.py
@@ -1,5 +1,12 @@
-from flask import Blueprint, redirect, request, render_template
+import os
+import secrets
+from flask import Blueprint, redirect, request, render_template, url_for
 from Backend import From_File
+from flask_login import current_user, login_required
+from form import UpdateAccountForm
+from __init__ import db, bcrypt, app
+from PIL import Image
+from models import user as DBUser
 
 # from Backend import From_File
 
@@ -21,6 +28,41 @@ def check():
         # redirect("/check")
         # os.system(f"Backend\From_File.py -f {a}")
     return f"<h1>{a}</h1>"
+
+
+def save_pic(pic):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(pic.filename)
+    pic_fn = random_hex + f_ext
+    pic_path = os.path.join(app.root_path, "static/Assets/userimage", pic_fn)
+    pic.save(pic_path)
+    output_size = (125, 125)
+    i = Image.open(pic)
+    i.thumbnail(output_size)
+    i.save(pic_path)
+    return pic_fn
+
+
+@views.route("/<string:username>", methods=["GET", "POST"])
+@login_required
+def account(username):
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_pic(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        db.session.commit()
+        return redirect(url_for("views.home"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+
+    if DBUser.query.filter_by(username=username).first():
+        return render_template(
+            "UserProfile/Account.html", title=current_user.username, form=form
+        )
+    else:
+        return render_template("404/pagenotfound.html", title="Pagenotfound")
 
 
 @views.route("/Song")
